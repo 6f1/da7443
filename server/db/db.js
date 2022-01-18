78ad058f20a50680015f41d604f54ec2a9f73375
Sam Shoemaker <sshoemaker@protoplay.tv>
2022-01-18 14:54:54 -0500

    Solutions for message posting and ordering

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://sam:ZJ+0Hm5WwJU77bc0@localhost:5432/messenger", {
   logging: false
 });
 
