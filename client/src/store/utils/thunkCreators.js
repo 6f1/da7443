c392a1611b9266504c2ab5f1152efd32be2ae670
6f1 <84033449+6f1@users.noreply.github.com>
2022-01-20 10:50:19 -0500

    Solutions for message posting and ordering (#2)
    * Solutions for message posting and ordering
    * Changes to code structure to conform to standards
    
    Co-authored-by: Sam Shoemaker <sshoemaker@protoplay.tv>

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -72,6 +72,9 @@ export const logout = (id) => async (dispatch) => {
 export const fetchConversations = () => async (dispatch) => {
   try {
     const { data } = await axios.get("/api/conversations");
+    for ( const convo of data ) {
+        convo.messages.sort((a,b) => new Date(a.createdAt) - new Date(b.createdAt));
+    }
     dispatch(gotConversations(data));
   } catch (error) {
     console.error(error);
@@ -93,16 +96,14 @@ const sendMessage = (data, body) => {
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
-export const postMessage = (body) => (dispatch) => {
+export const postMessage = (body) => async (dispatch) => {
   try {
-    const data = saveMessage(body);
-
+    const data = await saveMessage(body);
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
     } else {
       dispatch(setNewMessage(data.message));
     }
-
     sendMessage(data, body);
   } catch (error) {
     console.error(error);
