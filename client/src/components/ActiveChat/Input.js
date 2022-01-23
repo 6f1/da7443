8c109b75015e416f7849a70f84bdbaf502b45010
Sam Shoemaker <sshoemaker@protoplay.tv>
2022-01-23 16:23:41 -0500

    New feature almost completely implemented

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,26 +1,56 @@
-import React, { useState } from "react";
-import { FormControl, FilledInput } from "@material-ui/core";
+import React, { useState, useRef } from "react";
+import { FormControl, FilledInput, Chip } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import { postMessage } from "../../store/utils/thunkCreators";
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     justifySelf: "flex-end",
     marginTop: 15
+  },
+  inputContainer: {
+    display: 'inline-flex'
   },
   input: {
     height: 70,
     backgroundColor: "#F4F6FA",
     borderRadius: 8,
     marginBottom: 20
+  },
+  inputOverlay: {
+    position: 'absolute',
+    right: '1rem',
+    top: '1.5rem',
+    cursor: 'pointer',
+    color: "#BECCE2"
   }
 }));
 
 const Input = (props) => {
   const classes = useStyles();
   const [text, setText] = useState("");
+  const fileInputRef = useRef(null);
+  const [ attachments, setAttachments ] = useState([]);
   const { postMessage, otherUser, conversationId, user } = props;
+
+  const getCloudinaryUrl = async (file) => {
+    const apiKey = '256692893972532', preset = 'gs64sqvm', formData = new FormData();
+    formData.append('file', file);
+    formData.append('api_key', apiKey);
+    formData.append('upload_preset', preset);
+    try {
+      const res = await fetch(`https://api.cloudinary.com/v1_1/${preset}/image/upload`, {
+        method: 'POST',
+        body: formData
+      });
+      const { secure_url } = await res.json();
+      return secure_url;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleChange = (event) => {
     setText(event.target.value);
@@ -28,20 +58,34 @@ const Input = (props) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    // upload to cloudinary
+    let links = [];
+    for (const file of attachments) {
+      // get cloudinary links here
+      const newLink = await getCloudinaryUrl(file);
+      links.push(newLink);
+    }
+
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
     const reqBody = {
       text: event.target.text.value,
       recipientId: otherUser.id,
       conversationId,
-      sender: conversationId ? null : user
+      sender: conversationId ? null : user,
+      attachments: links
     };
     await postMessage(reqBody);
     setText("");
+    setAttachments([])
   };
 
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
-      <FormControl fullWidth hiddenLabel>
+      {attachments.map((att, index) => (
+        <Chip label={att.name} onDelete={() => setAttachments(attachments.filter((file) => file.name !== att.name))} key={index}/>
+      ))}
+      <FormControl fullWidth hiddenLabel className={classes.inputContainer}>
         <FilledInput
           classes={{ root: classes.input }}
           disableUnderline
@@ -49,6 +93,13 @@ const Input = (props) => {
           value={text}
           name="text"
           onChange={handleChange}
+        />
+        <FileCopyOutlinedIcon onClick={() => fileInputRef && fileInputRef.current && fileInputRef.current.click()} className={classes.inputOverlay}/>
+        <input
+          type="file"
+          style={{ display: 'none' }}
+          ref={fileInputRef}
+          onChange={(e) => setAttachments([...attachments, ...e.target.files])}
         />
       </FormControl>
     </form>
--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Grid } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -19,22 +19,53 @@ const useStyles = makeStyles(() => ({
     color: "#91A3C0",
     letterSpacing: -0.2,
     padding: 8,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    borderRadius: "10px 10px 0 10px"
   },
   bubble: {
     background: "#F4F6FA",
-    borderRadius: "10px 10px 0 10px"
+  },
+  senderUserContent: {
+    borderRadius: '10px 10px 0 10px',
+    overflow: 'hidden'
+  },
+  imgBubble: {
+    maxWidth: '10rem',
+    maxHeight: '10rem',
+    overflow: 'hidden',
+  },
+  imgBubbles: {
+    maxWidth: '5rem',
+    maxHeight: '5rem',
+    overflow: 'hidden',
+    borderRadius: '10px 10px 0 10px',
+    marginLeft: '.5rem'
+  },
+  senderImg: {
+    objectFit: 'cover',
+    height: '100%',
   }
 }));
 
 const SenderBubble = (props) => {
   const classes = useStyles();
-  const { time, text } = props;
+  const { time, text, attachments } = props;
   return (
     <Box className={classes.root}>
       <Typography className={classes.date}>{time}</Typography>
-      <Box className={classes.bubble}>
-        <Typography className={classes.text}>{text}</Typography>
+      <Box className={classes.senderUserContent}>
+        <Grid container direction="row" justifyContent="flex-end">
+          {attachments && attachments.length > 0 && attachments.map((att, index) => (
+            <Box className={attachments.length > 1 ? classes.imgBubbles : classes.imgBubble}>
+              <img src={att} className={classes.senderImg} alt="Your image"/>
+            </Box>
+          ))}
+        </Grid>
+        {text && (
+          <Box className={classes.bubble}>
+            <Typography className={classes.text}>{text}</Typography>
+          </Box>
+        )}
       </Box>
     </Box>
   );
