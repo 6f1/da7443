f90acd162f96306983afa547c94598350b759d49
Sam Shoemaker <sshoemaker@protoplay.tv>
2022-01-27 11:42:20 -0500

    Add in final change required from PR review

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -58,11 +58,7 @@ const Input = (props) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    let links = [];
-    for (const file of attachments) {
-      const newLink = await getCloudinaryUrl(file);
-      links.push(newLink);
-    }
+    const links = await Promise.all(attachments.map((file) => getCloudinaryUrl(file)));
 
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
     const reqBody = {
